a0e213b12eabf435797c2a6953a7d55e9e1c569e
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-10-04 03:26:48 -0400

    initial commit :tada:

---
+++ controllers/coinInput.js
@@ -0,0 +1,8 @@
+// make user schema promises
+var Promise = require('bluebird');
+var User = Promise.promisifyAll(require('../models/userModel'));
+
+// add coin to database
+module.exports = function(req, res) {
+
+}
