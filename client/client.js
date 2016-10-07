7420fa91c6d302537a6e6826fec5cf3bc7e10fff
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-10-06 22:42:20 -0400

    remames client.js to index.jsx & refactor for react routes

--- client/client.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from '../components/App';
-import 'react-toolbox/lib/commons.scss';
-
-render(
-	<App />, document.getElementById('app')
-);
