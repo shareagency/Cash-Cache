cc2ff1ac5c7598f8664cf29b96385304b0cb225f
adam fader <adamfader@gmail.com>
2016-10-07 05:54:36 -0400

    added linkContainer to login link

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Link } from 'react-router'
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap"
-import {LinkContainer} from 'react-router-bootstrap'
+import { Link } from 'react-router';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap";
+import {LinkContainer} from 'react-router-bootstrap';
 
 export default React.createClass({
   render() {
@@ -10,6 +10,7 @@ export default React.createClass({
       <div className="page-wrap">
         <div className="circle"></div>
         <img className="logo-dollar" src="assets/images/cc_grade.png" />
+        <span className="logo-text">CASH CACHE</span>
 
         <nav className="header navbar navbar-inverse navbar-fixed-top" role="navigation">
 
@@ -21,13 +22,14 @@ export default React.createClass({
             </button>
 
             <div className="head-wrap">
-              <span className="logo-text">CASH CACHE</span>
 
               <div id="navbar" className="navbar-collapse collapse cash-nav-wrap float-right">
                   <a className="cash-nav" href="/">Home</a>
                   <a className="cash-nav" href="/about">About</a>
                   <a className="cash-nav" href="/tools">Tools</a>
-                  <a className="cash-nav" href="/login">Log In</a>
+                  <LinkContainer to={{ pathname: '/login'}}>
+                    <a className="cash-nav">Log In</a>
+                  </LinkContainer>
                   <a className="cash-nav" href="/signup">Sign Up</a>
               </div>
             </div>
@@ -42,7 +44,11 @@ export default React.createClass({
           <li role="separator" className="divider"></li>
           <li className="li-pad"><a className="resp-cash-nav" href="/tools">Tools</a></li>
           <li role="separator" className="divider"></li>
-          <li className="li-pad"><a className="resp-cash-nav" href="/login">Log In</a></li>
+          <li className="li-pad">
+            <LinkContainer to={{ pathname: '/login'}}>
+              <a className="resp-cash-nav">Log In</a>
+            </LinkContainer>
+          </li>
           <li role="separator" className="divider"></li>
           <li className="li-pad"><a className="resp-cash-nav" href="/signup">Sign Up</a></li>
         </ul>
