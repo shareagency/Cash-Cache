b06618f70c0706db3a3e773117361d14343c8b38
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-10-12 00:22:01 -0400

    gets user data for tools page

---
+++ controllers/coinController.js
@@ -0,0 +1,40 @@
+var User = require('../models/userModel');
+
+module.exports = function(req, res) {
+  let username = req.user.username;
+  console.log('Coins requested');
+  if (!username) {
+    res.json({
+      err: 'could not determin username',
+      errMsg: 'Username is undefined'
+    });
+    return;
+  }
+
+  User.findOne({username: username}).exec()
+  // add inserted coin
+  .then(function(user) {
+    let newUser = {};
+    let coins = user.coins;
+    newUser.coins = [coins.pennies, coins.nickels, coins.dimes, coins.quarters]
+    newUser.username = user.username;
+    return newUser;
+  })
+  .then(function(newUser) {
+    console.log('User: ', newUser);
+    res.json({
+      status: 'ok',
+      userData: newUser
+    });
+  })
+  // catch any errors
+  .catch(function(err) {
+    console.log('ERROR: ', err);
+    res.json({
+      status: 'error',
+      errMsg: 'Query err: ' + err
+    });
+  })
+  
+
+};
