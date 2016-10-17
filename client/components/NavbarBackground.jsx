1367fc8dc378ce0e0712c471b593961d391b4b09
adam fader <adamfader@gmail.com>
2016-10-17 16:34:38 -0400

    tweaked animation height and z-indexes

--- client/components/NavbarBackground.jsx
+++ client/components/NavbarBackground.jsx
@@ -4,21 +4,60 @@ import '../theme/NavbarBackground.scss';
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 import auth from './utils/helpers/requests';
 import uuid from 'node-uuid';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class NavbarBackground extends Component {
 
   constructor() {
     super();
+    this.state = {
+      coins: []
+    };
   }
 
   logout() {
     auth.logout.call(auth)
   }
 
+  handleAdd(event) {
+    const coinVal = event.target.id;
+    const newCoins = [];
+    newCoins.push(coinVal);    
+    this.setState({coins: newCoins});
+  }
+
+  handleRemove(i) {
+    const newCoins = this.state.coins.slice();
+    newCoins.splice(i, 1);
+    this.setState({coins: newCoins});
+  }
+
+  componentDidUpdate() {
+    const coinCheck = this.state.coins[0];
+
+    if(coinCheck == 'penny' || coinCheck == 'nickel' || coinCheck == 'dime' || coinCheck == 'quarter'){
+      this.handleRemove(0);
+    }  
+  }
+
   render() {
+    const coins = this.state.coins.map((coin, i) => (
+      <div key={coin} id="coin-img" className={coin}></div>
+    ));
 
     return (
       <div>
+        <ReactCSSTransitionGroup
+          transitionName={ {
+            enter: 'enter',
+            enterActive: 'enterActive',
+            leave: 'leave',
+            leaveActive: 'leaveActive'
+          } }
+          transitionEnterTimeout={800}
+          transitionLeaveTimeout={800}>
+          {coins}
+        </ReactCSSTransitionGroup>
         <Navbar inverse>
           <Navbar.Header>
             <Navbar.Brand>
@@ -52,10 +91,10 @@ export default class NavbarBackground extends Component {
                   </LinkContainer>
                 ]
               )}
+              
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-
         {/* Background images */}
         <img id="piggy-front" src="assets/images/piggy_front.png" />
         <img id="piggy" src="assets/images/piggy_light.png" />
